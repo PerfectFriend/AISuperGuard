4a7b091f8158b6da16b80d1a32de79bbed1a8e49
DarkPushkin <master@grimoire.local>
2026-08-08 10:32:50 +0200

    chore: add .gitignore for egg-info and test files

--- kill_zombies.ps1
+++
@@ -1,4 +0,0 @@
-$mypid = 18220
-Get-CimInstance Win32_Process -Filter "Name='python.exe'" |
-Where-Object { $_.CommandLine -match '(panic_mode|main\.py|superguard)' -and $_.ProcessId -ne $mypid } |
-ForEach-Object { Stop-Process -Id $_.ProcessId -Force; Write-Output ('killed ' + $_.ProcessId) }
